5cb0adb3edf97d467309cd16a012131ee80f4905
Vikas Kumar <feel.the.coder@gmail.com>
2021-04-13 17:23:43 +0530

    website completed

--- js/custom.js
+++ js/custom.js
@@ -17,72 +17,62 @@ $(window).load(function() {
 /**== Menu js ==**/
 
 $("#navbar_menu").menumaker({
-	title: "Menu",
-	format: "multitoggle"
+    title: "Menu",
+    format: "multitoggle"
 });
 
 /** counter js **/
 
-    $('.counter-count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 5000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
+$('.counter-count').each(function() {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function(now) {
+            $(this).text(Math.ceil(now));
+        }
     });
+});
 
 /** progress_bar js **/
-	
-	 $(document).ready(function() {
-      $('.progress .progress-bar').css("width",
-                function() {
-                    return $(this).attr("aria-valuenow") + "%";
-                }
-        )
-    });
-	
-/** Casestudies Tab_bar js **/	
-	
-	$(document).ready(function(){
 
-    $(".filter-button").click(function(){
+$(document).ready(function() {
+    $('.progress .progress-bar').css("width",
+        function() {
+            return $(this).attr("aria-valuenow") + "%";
+        }
+    )
+});
+
+/** Casestudies Tab_bar js **/
+
+$(document).ready(function() {
+
+    $(".filter-button").click(function() {
         var value = $(this).attr('data-filter');
-        
-        if(value == "all")
-        {
+
+        if (value == "all") {
             //$('.filter').removeClass('hidden');
             $('.filter').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-            
+        } else {
+            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.' + value).hide('3000');
+            $('.filter').filter('.' + value).show('3000');
+
         }
     });
-    
+
     if ($(".filter-button").removeClass("active")) {
-$(this).removeClass("active");
-}
-$(this).addClass("active");
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
 
 });
 
 /** google_map js **/
 
-function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(40.712775,-74.005973),
-    zoom:18,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
 
 /**===== Slider =====**/
 
@@ -186,19 +176,14 @@ tpj(document).ready(function() {
 });
 
 /**===== End slider =====**/
-	
-	
+
+
 /** header fixed js **/
 
-$(window).scroll(function(){
+$(window).scroll(function() {
     if ($(window).scrollTop() >= 300) {
-       $('.header_fixed_on_scroll').addClass('fixed-header');
-    }
-    else {
-       $('.header_fixed_on_scroll').removeClass('fixed-header');
+        $('.header_fixed_on_scroll').addClass('fixed-header');
+    } else {
+        $('.header_fixed_on_scroll').removeClass('fixed-header');
     }
 });
-
-	
-	
-	
